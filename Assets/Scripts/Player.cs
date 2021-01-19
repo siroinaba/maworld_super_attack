bbf4d1c8eb236544818c2f978c4c947fc5b755a3
Momendohu <pinog.jh@gmail.com>
2021-01-19 22:26:13 +0900

    feat:色々消したり作ったり

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    void Awake(){
+        
+    }
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
